57804e71e6cefa74f0f8d6b0d64bdc2b5d987d6d
mrliedson <alissonbrendo99@gmail.com>
2024-11-18 21:27:13 -0300

    Começo do projeto, criando interface

---
+++ Empresa_de_Telefone/MainForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aluno
+ * Date: 18/11/2024
+ * Time: 21:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Empresa_de_Telefone
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// MainForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "Empresa_de_Telefone";
+			this.Name = "MainForm";
+		}
+	}
+}
