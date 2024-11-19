dad1ccc9771d6b01996e68d8267ded07a05732eb
mrliedson <alissonbrendo99@gmail.com>
2024-11-18 22:30:28 -0300

    Criada insterface

--- Empresa_de_Telefone/MainForm.Designer.cs
+++ Empresa_de_Telefone/MainForm.Designer.cs
@@ -14,6 +14,20 @@ namespace Empresa_de_Telefone
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -36,12 +50,186 @@ namespace Empresa_de_Telefone
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.BackColor = System.Drawing.Color.SteelBlue;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(52, 379);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(133, 63);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Adicionar";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(285, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(229, 37);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "TELEFONICA";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(42, 114);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(148, 35);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(42, 207);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(148, 35);
+			this.textBox2.TabIndex = 3;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(42, 300);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(148, 35);
+			this.textBox3.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(42, 81);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 30);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Nome:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(42, 174);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(110, 29);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Telefone";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(42, 267);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(69, 29);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Valor";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(236, 97);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(314, 345);
+			this.richTextBox1.TabIndex = 8;
+			this.richTextBox1.Text = "";
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(570, 97);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(314, 345);
+			this.richTextBox2.TabIndex = 9;
+			this.richTextBox2.Text = "";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(298, 65);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(196, 29);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "FUNCIONÁRIOS";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(617, 65);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(204, 29);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "ACIMA DA MÉDIA";
+			// 
+			// button2
+			// 
+			this.button2.AutoSize = true;
+			this.button2.BackColor = System.Drawing.Color.SteelBlue;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(236, 457);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(145, 63);
+			this.button2.TabIndex = 12;
+			this.button2.Text = "SALVAR";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this.button3.AutoSize = true;
+			this.button3.BackColor = System.Drawing.Color.SteelBlue;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.ForeColor = System.Drawing.Color.White;
+			this.button3.Location = new System.Drawing.Point(387, 457);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(163, 63);
+			this.button3.TabIndex = 13;
+			this.button3.Text = "CARREGAR";
+			this.button3.UseVisualStyleBackColor = false;
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Empresa_de_Telefone";
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(931, 600);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
+			this.Text = "Empresa_de_Telefone";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 	}
 }
--- Empresa_de_Telefone/MainForm.cs
+++ Empresa_de_Telefone/MainForm.cs
@@ -1,33 +1,23 @@
-﻿/*
- * Created by SharpDevelop.
- * User: aluno
- * Date: 18/11/2024
- * Time: 21:19
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace Empresa_de_Telefone
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
+
 	public partial class MainForm : Form
 	{
 		public MainForm()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
+
 			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
+
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			richTextBox1.Text += textBox1.Text + " | " + textBox2.Text + " | " + textBox3.Text + "\n";
+	
 		}
 	}
 }
